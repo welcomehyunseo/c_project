a90bcb59679164f31977168d584b1cfe8cb6743e
welcomehyunseo <welcomehyunseo@gmail.com>
2023-06-01 15:52:58 +0900

    save

--- src/main.c
+++ src/main.c
@@ -3,19 +3,19 @@
 
 #define NORETURN __attribute__((__noreturn__))
 
-void exit_program() NORETURN;
+void call_error() NORETURN;
 
-void exit_program()
+void call_error() 
 {
-  printf("call exit_program()\n");
+  printf("there are something error\n");
   exit(EXIT_FAILURE);
+  // return;
 }
 
-// int main(int argc, char *argv[])
 int main()
 {
   printf("before\n");
-  exit_program();
+  call_error();
   printf("after\n");
 
   return 0;
